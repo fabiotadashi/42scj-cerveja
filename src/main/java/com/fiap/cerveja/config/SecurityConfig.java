e498adee964c3fc931af13df7a7e1afc469c2806
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2022-02-10 20:09:38 -0300

    dtos auth

--- src/main/java/com/fiap/cerveja/config/SecurityConfig.java
+++ src/main/java/com/fiap/cerveja/config/SecurityConfig.java
@@ -1,26 +1,48 @@
 package com.fiap.cerveja.config;
 
+import com.fiap.cerveja.security.JwtAutheticationEntryPoint;
+import com.fiap.cerveja.security.JwtRequestFilter;
+import com.fiap.cerveja.security.JwtUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private JwtUserDetailsService jwtUserDetailsService;
+    private PasswordEncoder passwordEncoder;
+    private JwtAutheticationEntryPoint jwtAutheticationEntryPoint;
+    private JwtRequestFilter jwtRequestFilter;
+
+    public SecurityConfig(JwtUserDetailsService jwtUserDetailsService,
+                          PasswordEncoder passwordEncoder,
+                          JwtAutheticationEntryPoint jwtAutheticationEntryPoint,
+                          JwtRequestFilter jwtRequestFilter) {
+        this.jwtUserDetailsService = jwtUserDetailsService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtAutheticationEntryPoint = jwtAutheticationEntryPoint;
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
     // Autenticação (401 - Unauthorized)
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("fiapadmin").password("{noop}admin").roles("ADMIN","USER")
-                    .and()
-                .withUser("fiapuser").password("{noop}senha").roles("USER");
+        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder);
     }
 
     // Autorização (403 - Forbidden)
@@ -29,9 +51,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.httpBasic()
                 .and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/cervejas").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.POST, "/cervejas").hasRole("ADMIN")
+                .antMatchers("/usuarios/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
+                    .exceptionHandling().authenticationEntryPoint(jwtAutheticationEntryPoint)
+                .and()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
                 .csrf().disable()
                 .formLogin().disable();
     }
@@ -50,5 +78,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 );
     }
 
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
 }
